782ee7218af64313dd9e27bba3b0c63e74cbeebb
jp0617 <juan.ramirez6323@unaula.edu.co>
2026-05-13 21:14:17 -0500

    se agrega servicio

--- index.py
+++ index.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import pandas as pd
 from modelo.predict import init, predict, top5, team_stats
+from modelo.montecarlo import print_json_prediction
+from modelo.hibrido.main import prediccion_personalizada
 
 app = Flask(__name__)
+CORS(app)
 
 init()
 
@@ -16,8 +20,14 @@ def home():
 @app.route('/team/<string:teamhome>/<string:teamaway>', methods=['GET'])
 def disp(teamhome, teamaway):
     try:
-        result = predict(teamhome, teamaway)
-        return jsonify(result)
+        result = print_json_prediction(teamhome, teamaway)
+        result2 = predict(teamhome, teamaway)
+        resultLogit = prediccion_personalizada(teamhome, teamaway)
+        return {
+            "montecarlo": result,
+            "predict": result2,
+            "logit&possion": resultLogit
+        }
     except ValueError as e:
         return jsonify({'error': str(e)}), 404
 
@@ -32,4 +42,4 @@ def team_stats_endpoint(team):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
