476447aef141c7696c80fc52a858695008000cb5
jp0617 <juan.ramirez6323@unaula.edu.co>
2026-05-15 12:42:01 -0500

    se arreglo

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import pandas as pd
 from modelo.predict import init, predict, top5, team_stats
 from modelo.montecarlo import print_json_prediction
-from modelo.hibrido.main import prediccion_personalizada
+#from modelo.hibrido.main import prediccion_personalizada
 
 app = Flask(__name__)
 CORS(app)
@@ -22,11 +22,11 @@ def disp(teamhome, teamaway):
     try:
         result = print_json_prediction(teamhome, teamaway)
         result2 = predict(teamhome, teamaway)
-        resultLogit = prediccion_personalizada(teamhome, teamaway)
+        #resultLogit = prediccion_personalizada(teamhome, teamaway)
         return {
             "montecarlo": result,
             "predict": result2,
-            "logit&possion": resultLogit
+            #"logit&possion": resultLogit
         }
     except ValueError as e:
         return jsonify({'error': str(e)}), 404
@@ -40,6 +40,3 @@ def team_stats_endpoint(team):
     except ValueError as e:
         return jsonify({'error': str(e)}), 404
 
-
-if __name__ == '__main__':
-    app.run(debug=True,port=5001)
